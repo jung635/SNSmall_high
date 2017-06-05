3bbfdb21c9f3b72c958911746fe7b3f373e1ff59
sunju <sunju635@naver.com>
2017-06-05 14:42:05 +0900

    live update

--- SNSmall_high/WebContent/live/chatting.jsp
+++ SNSmall_high/WebContent/live/chatting.jsp
@@ -20,36 +20,33 @@
     <textarea id="messageTextArea" rows="10" cols="50"></textarea>
      
     <script type="text/javascript">
-        //WebSocketEx는 프로젝트 이름
-        //websocket 클래스 이름
-        var webSocket = new WebSocket("ws://sunju635.cafe24.com/SNSmall_high/websocket");
+        var webSocket = new WebSocket("ws://localhost:8080/SNSmall_high/websocket");
         var messageTextArea = document.getElementById("messageTextArea");
-        //웹 소켓이 연결되었을 때 호출되는 이벤트
+
         webSocket.onopen = function(message){
             messageTextArea.value += "Server connect...\n";
         };
-        //웹 소켓이 닫혔을 때 호출되는 이벤트
+
         webSocket.onclose = function(message){
             messageTextArea.value += "Server Disconnect...\n";
         };
-        //웹 소켓이 에러가 났을 때 호출되는 이벤트
+
         webSocket.onerror = function(message){
             messageTextArea.value += "error...\n";
         };
-        //웹 소켓에서 메시지가 날라왔을 때 호출되는 이벤트
+
         webSocket.onmessage = function(message){
             messageTextArea.value += "Recieve From Server => "+message.data+"\n";
         };
-        //Send 버튼을 누르면 실행되는 함수
+
         function sendMessage(){
             var message = document.getElementById("textMessage");
             messageTextArea.value += "Send to Server => "+message.value+"\n";
-            //웹소켓으로 textMessage객체의 값을 보낸다.
+           // message=["반갑습니다","test"];
             webSocket.send(message.value);
-            //textMessage객체의 값 초기화
+            //webSocket.send(message.value);
             message.value = "";
         }
-        //웹소켓 종료
         function disconnect(){
             webSocket.close();
         }
--- SNSmall_high/src/web/live/action/ChatAction.java
+++ SNSmall_high/src/web/live/action/ChatAction.java
@@ -1,40 +1,67 @@
 package web.live.action;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
 @ServerEndpoint("/websocket")
 public class ChatAction{
-	
-	 @OnOpen
-	    public void handleOpen(){
+	static List<Session> sessionUsers = Collections.synchronizedList(new ArrayList<>());
+
+		@OnOpen
+	    public void handleOpen(Session session){
 	        System.out.println("client is now connected...");
+			System.out.println("session:"+session.toString());
+			clients.add(session);
 	    }
-	    /**
-	     * �� �������κ��� �޽����� ���� ȣ��Ǵ� �̺�Ʈ
-	     * @param message
-	     * @return
-	     */
 	    @OnMessage
-	    public String handleMessage(String message){
-	        System.out.println("receive from client : "+message);
-	        String replymessage = "echo "+message;
-	        System.out.println("send to client : "+replymessage);
-	        return replymessage;
+	    public void handleMessage(String message, Session session){
+	       
+/*	        String replymessage = "";
+	        
+	        synchronized (clients) {
+				// Iterate over the connected sessions
+				// and broadcast the received message
+				//for (Session client : clients) {
+					//if (!client.equals(session)) {
+						 System.out.println("receive from client : "+message);
+						replymessage = "echo "+message;
+						System.out.println("send to client : "+replymessage);
+					//}
+				//}
+			}
+	        return replymessage;*/
+	    	 String username = (String)userSession.getUserProperties().get("username");
+	         //세션 프로퍼티에 username이 없으면 username을 선언하고 해당 세션을으로 메시지를 보낸다.(json 형식이다.)
+	         //최초 메시지는 username설정
+	         if(username == null){
+	             userSession.getUserProperties().put("username", message);
+	             userSession.getBasicRemote().sendText(buildJsonData("System", "you are now connected as " + message));
+	             return;
+	         }
+	         //username이 있으면 전체에게 메시지를 보낸다.
+	         Iterator<Session> iterator = sessionUsers.iterator();
+	         while(iterator.hasNext()){
+	             iterator.next().getBasicRemote().sendText(buildJsonData(username,message));
+	         }
+
+
+	 출처: http://nowonbun.tistory.com/286 [명월 일지]
 	    }
-	    /**
-	     * �� ������ ������ ȣ��Ǵ� �̺�Ʈ
-	     */
 	    @OnClose
-	    public void handleClose(){
+	    public void handleClose(Session session){
+	    	clients.remove(session);
 	        System.out.println("client is now disconnected...");
 	    }
-	    /**
-	     * �� ������ ������ ���� ȣ��Ǵ� �̺�Ʈ
-	     * @param t
-	     */
 	    @OnError
 	    public void handleError(Throwable t){
 	        t.printStackTrace();
